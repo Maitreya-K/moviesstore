d368d80aee804420282badd8994ebd4869f6ea5b
Maitreya-K <maitreya.kayyar@gmail.com>
2025-09-16 00:40:21 +0000

    Python Anywhere

--- moviesstore/settings.py
+++ moviesstore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-sa!all!^8&&@l(yc&-ubh9hnd&=9=o_y9hp#+2_m)%blqkhpsr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
